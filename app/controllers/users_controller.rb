d281474a073d53ad8c7709df93aff7b79b7b4d73
Dennis B <bluz71@users.noreply.github.com>
2015-09-27 16:15:43 +1000

    Finish log in and log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
+      # redirect_to will result in a new HTTP request. In this case a 
+      # 'user with-ID GET' to users#show (aka the show method above).
       redirect_to @user
     else
+      # render the 'new' view, unlike redirect_to, will be handled in this same
+      # request.
       render 'new'
     end
   end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,51 @@
+module SessionsHelper
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Remember a user in a persistent session.
+  def remember(user)
+    # Create the user's remember token and save remember digest to the
+    # database.
+    user.remember
+    # Securely store the user id in a cookie, with 20 year expiry.
+    cookies.permanent.signed[:user_id] = user.id
+    # Store the user's remember token in a cookie also with 20 year expiry.
+    # Note, this cookie value is only valid until a user logouts since each
+    # time a user logs in a new remember token will get generated.
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # Forget a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Logs out the current user.
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
